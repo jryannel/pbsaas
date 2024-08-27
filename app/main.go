646e6346f8d4424190e8b8acce3868fa4440c533
Jürgen Ryannel <juergen@ryannel.org>
2024-08-27 09:05:13 +0200

    initial app

---
+++ app/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+	"os"
+	"strings"
+
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/plugins/migratecmd"
+	_ "go.uber.org/automaxprocs"
+)
+
+func main() {
+	app := pocketbase.New()
+	// loosely check if it was executed using "go run"
+	isGoRun := strings.HasPrefix(os.Args[0], os.TempDir())
+	migratecmd.MustRegister(app, app.RootCmd, migratecmd.Config{
+		// enable auto creation of migration files when making collection changes in the Admin UI
+		// (the isGoRun check is to enable it only during development)
+		Automigrate: isGoRun,
+	})
+	err := app.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
